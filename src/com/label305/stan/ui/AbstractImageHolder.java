e51ee585b91bfc1da83efcac28ced008a08ef305
Niek Haarman <niek@label305.com>
2013-04-18 16:49:48 +0200

    Added AbstractImageHolder

---
+++ src/com/label305/stan/ui/AbstractImageHolder.java
@@ -0,0 +1,28 @@
+package com.label305.stan.ui;
+
+import com.label305.stan.asyncutils.ImageLoader;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+public abstract class AbstractImageHolder extends LinearLayout {
+
+	public AbstractImageHolder(Context context) {
+		super(context);
+	}
+
+	public AbstractImageHolder(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	protected abstract ImageView getImageView();
+
+	protected abstract ProgressBar getProgressBar();
+
+	public void setImageUrl(String uri) {
+		ImageLoader.loadImage(uri, getProgressBar(), getImageView());
+	}
+}
