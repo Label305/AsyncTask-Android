2d9b7ac1e492c70fbdcd688d85653f2c6554bd57
Niek Haarman <niek@label305.com>
2013-04-26 14:57:32 +0200

    Created SimpleImageHolder

---
+++ src/com/label305/stan/ui/widget/SimpleImageHolder.java
@@ -0,0 +1,37 @@
+package com.label305.stan.ui.widget;
+
+import com.label305.stan.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class SimpleImageHolder extends AbstractImageHolder {
+
+	public SimpleImageHolder(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public SimpleImageHolder(Context context) {
+		super(context);
+		init();
+	}
+
+	private void init() {
+		LayoutInflater.from(getContext()).inflate(R.layout.simpleimageholder, this, true);
+	}
+
+	@Override
+	protected ImageView getImageView() {
+		return (ImageView) findViewById(R.id.stan_simpleimageholder_imageview);
+	}
+
+	@Override
+	protected ProgressBar getProgressBar() {
+		return (ProgressBar) findViewById(R.id.stan_simpleimageholder_progressbar);
+	}
+
+}
