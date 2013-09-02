b7bbeee0ce524d9f16d97bc140e2846e2d27c763
Niek Haarman <niek@label305.com>
2013-09-02 13:47:39 +0200

    Extracted TightWrapperCustomFontTextView out of CustomFontTextView

--- src/com/label305/stan/ui/widget/CustomFontTextView.java
+++ src/com/label305/stan/ui/widget/CustomFontTextView.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.text.Layout;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -72,28 +71,5 @@ public class CustomFontTextView extends TextView implements CustomFontInterface 
 
 	public boolean shouldLowercase() {
 		return mShouldLowercase;
-	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-		Layout layout = getLayout();
-		if (layout != null) {
-			int width = (int) Math.ceil(getMaxLineWidth(layout)) + getCompoundPaddingLeft() + getCompoundPaddingRight();
-			int height = getMeasuredHeight();
-			setMeasuredDimension(width, height);
-		}
-	}
-
-	private float getMaxLineWidth(Layout layout) {
-		float max_width = 0.0f;
-		int lines = layout.getLineCount();
-		for (int i = 0; i < lines; i++) {
-			if (layout.getLineWidth(i) > max_width) {
-				max_width = layout.getLineWidth(i);
-			}
-		}
-		return max_width;
 	}
 }
