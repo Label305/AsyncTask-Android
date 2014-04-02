6a65821018de39d894911c432cbcf4b3ef66fd45
Niek Haarman <niek@label305.com>
2014-04-02 11:19:58 +0200

    Spaces instead of tabs

--- app/src/main/java/com/label305/stan/utils/Logger.java
+++ app/src/main/java/com/label305/stan/utils/Logger.java
@@ -1,73 +1,73 @@
 package com.label305.stan.utils;
 
-import com.crashlytics.android.Crashlytics;
-import com.label305.stan.BuildConfig;
-
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
+import com.label305.stan.BuildConfig;
 
 /**
  * A logger class which only logs when the app is in debug mode. Uses the
  * (modifiable) tag "StanLogger".
  */
 public class Logger {
-	public static String TAG = "StanLogger";
+    public static String TAG = "StanLogger";
 
-	public static void log(String msg) {
-		if (isDebug()) {
-			Log.v(TAG, msg);
-			Crashlytics.log(msg);
-		}
-	}
+    public static void log(String msg) {
+        if (isDebug()) {
+            Log.v(TAG, msg);
+            Crashlytics.log(msg);
+        }
+    }
 
-	public static void log(int msg) {
-		log(String.valueOf(msg));
-	}
+    public static void log(int msg) {
+        log(String.valueOf(msg));
+    }
 
-	public static void log(Exception e) {
-		log(null, e);
-	}
+    public static void log(Exception e) {
+        log(null, e);
+    }
 
-	public static void log(Context context, Object msg) {
-		if (msg == null) {
-			msg = "null";
-		}
+    public static void log(Context context, Object msg) {
+        if (msg == null) {
+            msg = "null";
+        }
 
-		if (msg.toString().trim().equals(""))
-			msg = "No message";
+        if (msg.toString().trim().equals(""))
+            msg = "No message";
 
-		if (isDebug()) {
-			Log.v(TAG, msg.toString());
-			Crashlytics.log(msg.toString());
+        if (isDebug()) {
+            Log.v(TAG, msg.toString());
+            Crashlytics.log(msg.toString());
 
-			if (context != null) {
-				try {
-					Toast.makeText(context, msg.toString(), Toast.LENGTH_SHORT).show();
-				} catch (Exception e) {
-				}
-			}
-		}
-	}
+            if (context != null) {
+                try {
+                    Toast.makeText(context, msg.toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
 
-	public static void log(Context context, Exception e) {
-		if (isDebug()) {
-			if (!(e instanceof InterruptedException)) {
-				e.printStackTrace();
+    public static void log(Context context, Exception e) {
+        if (isDebug()) {
+            if (!(e instanceof InterruptedException)) {
+                e.printStackTrace();
 
-				if (context != null) {
-					try {
-						Toast.makeText(context, e.getClass().getSimpleName() + ": " + e.getMessage(), Toast.LENGTH_SHORT).show();
-					} catch (Exception ex) {
-					}
-				}
-			} else {
-				Log.w(TAG, e.getClass().getSimpleName());
-			}
-		}
-	}
+                if (context != null) {
+                    try {
+                        Toast.makeText(context, e.getClass().getSimpleName() + ": " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    } catch (Exception ex) {
+                    }
+                }
+            } else {
+                Log.w(TAG, e.getClass().getSimpleName());
+            }
+        }
+    }
 
-	private static boolean isDebug() {
-		return BuildConfig.DEBUG;
-	}
+    private static boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
 }
