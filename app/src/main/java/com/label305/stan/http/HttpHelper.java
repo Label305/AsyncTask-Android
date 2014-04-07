9f8a3926cdb3906b6c8bd14cf71c1d3dc856bbdb
Niek Haarman <niek@label305.com>
2014-04-07 15:51:39 +0200

    Use a single instance of the AndroidHttpClient

--- app/src/main/java/com/label305/stan/http/HttpHelper.java
+++ app/src/main/java/com/label305/stan/http/HttpHelper.java
@@ -26,96 +26,82 @@ public class HttpHelper implements GetExecutor, PostExecutor, PutExecutor, Delet
 
     private static final String ANDROID = "Android";
 
+    /**
+     * The AndroidHttpClient to use. We use a single instance and for now, do not close it. // TODO: When should we??
+     */
+    private static final AndroidHttpClient HTTP_CLIENT = AndroidHttpClient.newInstance(ANDROID);
+
     @Override
     public HttpResponse delete(final String url, final Map<String, Object> headerData, final HttpEntity deleteDataEntity) throws IOException {
-        AndroidHttpClient httpClient = AndroidHttpClient.newInstance(ANDROID);
-        try {
-            HttpDeleteWithBody httpDelete = new HttpDeleteWithBody(url);
-            Iterator<String> keys = headerData.keySet().iterator();
-            Iterator<Object> values = headerData.values().iterator();
+        HttpDeleteWithBody httpDelete = new HttpDeleteWithBody(url);
+        Iterator<String> keys = headerData.keySet().iterator();
+        Iterator<Object> values = headerData.values().iterator();
 
-            while (keys.hasNext() && values.hasNext()) {
-                httpDelete.setHeader(keys.next(), values.next().toString());
-            }
-
-            if (deleteDataEntity != null) {
-                httpDelete.setEntity(deleteDataEntity);
-            }
-
-            AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpDelete);
-            return httpClient.execute(httpDelete);
-        } finally {
-            httpClient.close();
+        while (keys.hasNext() && values.hasNext()) {
+            httpDelete.setHeader(keys.next(), values.next().toString());
         }
+
+        if (deleteDataEntity != null) {
+            httpDelete.setEntity(deleteDataEntity);
+        }
+
+        AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpDelete);
+        return HTTP_CLIENT.execute(httpDelete);
     }
 
     @Override
     public HttpResponse get(final String url, final Map<String, Object> headerData) throws IOException {
-        AndroidHttpClient httpClient = AndroidHttpClient.newInstance(ANDROID);
-        try {
-            HttpUriRequest httpGet = new HttpGet(url);
-            Iterator<String> keys = headerData.keySet().iterator();
-            Iterator<Object> values = headerData.values().iterator();
+        HttpUriRequest httpGet = new HttpGet(url);
+        Iterator<String> keys = headerData.keySet().iterator();
+        Iterator<Object> values = headerData.values().iterator();
 
-            while (keys.hasNext() && values.hasNext()) {
-                httpGet.setHeader(keys.next(), values.next().toString());
-            }
-
-            AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpGet);
-            return httpClient.execute(httpGet);
-        } finally {
-            httpClient.close();
+        while (keys.hasNext() && values.hasNext()) {
+            httpGet.setHeader(keys.next(), values.next().toString());
         }
+
+        AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpGet);
+        return HTTP_CLIENT.execute(httpGet);
     }
 
     @Override
     public HttpResponse post(final String url, final Map<String, Object> headerData, final HttpEntity postDataEntity) throws IOException {
-        AndroidHttpClient httpClient = AndroidHttpClient.newInstance(ANDROID);
-        try {
-            HttpPost httpPost = new HttpPost(url);
+        HttpPost httpPost = new HttpPost(url);
 
-            Iterator<String> keys = headerData.keySet().iterator();
-            Iterator<Object> values = headerData.values().iterator();
+        Iterator<String> keys = headerData.keySet().iterator();
+        Iterator<Object> values = headerData.values().iterator();
 
-            while (keys.hasNext() && values.hasNext()) {
-                httpPost.setHeader(keys.next(), values.next().toString());
-            }
-
-            if (postDataEntity != null) {
-                httpPost.setEntity(postDataEntity);
-            }
-
-            AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpPost);
-
-            return httpClient.execute(httpPost);
-        } finally {
-            httpClient.close();
+        while (keys.hasNext() && values.hasNext()) {
+            httpPost.setHeader(keys.next(), values.next().toString());
         }
+
+        if (postDataEntity != null) {
+            httpPost.setEntity(postDataEntity);
+        }
+
+        AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpPost);
+
+        return HTTP_CLIENT.execute(httpPost);
     }
 
     @Override
     public HttpResponse put(final String url, final Map<String, Object> headerData, final HttpEntity putDataEntity) throws IOException {
-        AndroidHttpClient httpClient = AndroidHttpClient.newInstance(ANDROID);
-        try {
-            HttpPut httpPut = new HttpPut(url);
-            Iterator<String> keys = headerData.keySet().iterator();
-            Iterator<Object> values = headerData.values().iterator();
+        HttpPut httpPut = new HttpPut(url);
+        Iterator<String> keys = headerData.keySet().iterator();
+        Iterator<Object> values = headerData.values().iterator();
 
-            while (keys.hasNext() && values.hasNext()) {
-                httpPut.setHeader(keys.next(), values.next().toString());
-            }
-
-            if (putDataEntity != null) {
-                httpPut.setEntity(putDataEntity);
-            }
-
-            AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpPut);
-
-            return httpClient.execute(httpPut);
-        } finally {
-            httpClient.close();
+        while (keys.hasNext() && values.hasNext()) {
+            httpPut.setHeader(keys.next(), values.next().toString());
         }
+
+        if (putDataEntity != null) {
+            httpPut.setEntity(putDataEntity);
+        }
+
+        AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpPut);
+
+        return HTTP_CLIENT.execute(httpPut);
     }
+
 
     public static List<NameValuePair> convert(final Map<String, Object> data) {
         List<NameValuePair> results = new ArrayList<NameValuePair>();
