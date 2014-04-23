2703c531919f7114c886880fbe71adfa73c47184
Nick van den Berg <nick@label305.com>
2014-04-23 10:00:42 +0200

    rewrite

--- app/src/main/java/com/label305/stan/utils/HttpDeleteWithBody.java
+++ app/src/main/java/com/label305/stan/utils/HttpDeleteWithBody.java
@@ -1,20 +1,27 @@
 package com.label305.stan.utils;
 
-import java.net.URI;
-
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 
+import java.net.URI;
+
 public class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
-	public static final String METHOD_NAME = "DELETE";
-    public String getMethod() { return METHOD_NAME; }
+    public static final String METHOD_NAME = "DELETE";
+
+    public String getMethod() {
+        return METHOD_NAME;
+    }
 
     public HttpDeleteWithBody(final String uri) {
         super();
         setURI(URI.create(uri));
     }
+
     public HttpDeleteWithBody(final URI uri) {
         super();
         setURI(uri);
     }
-    public HttpDeleteWithBody() { super(); }
+
+    public HttpDeleteWithBody() {
+        super();
+    }
 }
